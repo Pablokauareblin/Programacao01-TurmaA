da5d3ccf91fce9c944f0fac5fc93a41b58ac0923
aluno <aluno@lab01-03>
2026-03-03 11:16:30 -0300

    Aula Programacao de vdd

---
+++ SistemaIfc/src/main/java/Ibirama/sistema/ifc/SistemaIfc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Ibirama.sistema.ifc;
+
+/**
+ *
+ * @author aluno
+ */
+public class SistemaIfc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
